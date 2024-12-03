0105fe36bccd78a42b4160ea801d44f6b1dd134e
MH Code <mazheriftikhar1@gmail.com>
2024-12-03 17:19:15 +0500

    3-Dec changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Contact1 from './Componenets/Contact1/Contact1'
+import Contact2 from './Componenets/Contact2/Contact2'
+import Contact3 from './Componenets/Contact3/Contact3'
+import Contact4 from './Componenets/Contact4/Contact4'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Contact1 />
+      <Contact2 />
+      <Contact3 />  
+      <Contact4 />
+
     </div>
   )
 }
