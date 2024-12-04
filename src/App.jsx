28fbd5c1224958832acb644bad070420050c081a
MH Code <mazheriftikhar1@gmail.com>
2024-12-04 11:42:20 +0500

    4-Nov changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Contact2 from './Componenets/Contact2/Contact2'
 import Contact3 from './Componenets/Contact3/Contact3'
 import Contact4 from './Componenets/Contact4/Contact4'
 import Contact5 from './Componenets/Contact5/Contact5'
+import Contact6 from './Componenets/Contact6/Contact6'
+import Contact7 from './Componenets/Contact7/Contact7'
+import Contact8 from './Componenets/Contact8/Contact8'
 
 
 const App = () => {
@@ -14,6 +17,9 @@ const App = () => {
       <Contact3 />  
       <Contact4 />
       <Contact5 />
+      <Contact6 />
+      <Contact7 />
+      <Contact8 />
     </div>
   )
 }
